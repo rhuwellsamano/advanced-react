a7cdb3d3c19ea44946bd731f94c46db3b3769120
rhuwellsamano <rhuwell.samano@gmail.com>
2019-10-09 15:24:39 -0400

    advanced react work

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+const Home = props => (
+    <div>
+        <p>HEY HEY!</p>
+        <Link href="/sell ">
+            <a>SELL</a>
+        </Link>
+    </div>
+)
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+ 
+const Sell = props => (
+    <div>
+        <p>SELL!</p>
+    </div>
+)
+
+export default Sell;
