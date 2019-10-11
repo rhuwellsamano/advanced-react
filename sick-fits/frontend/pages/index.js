f2d9823b4d66c93c6280a8ed4b1fec1be8a47d0f
rhuwellsamano <rhuwell.samano@gmail.com>
2019-10-11 00:35:05 -0400

    successfully rendered out the two example Items onto React using ALL_ITEMS_QUERY

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,8 @@
-import Link from 'next/link';
+import Items from '../components/Items';
 
 const Home = props => (
     <div>
-        <p>HEY HEY!</p>
-        <Link href="/sell ">
-            <a>SELL</a>
-        </Link>
+        <Items/>
     </div>
 )
 
