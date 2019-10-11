099194dbaa0235e25652efe2877abfdec27eca29
rhuwellsamano <rhuwell.samano@gmail.com>
2019-10-10 22:38:19 -0400

    setups mutations, queries, graphql files

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,11 +1,22 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        // create a dog!
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
+    // TODO: Check if they are logged in
+
+    async createItem(parent, args, ctx, info) {
+
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
     }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     // create a dog!
+    //     const newDog = { name: args.name };
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    // }
 };
 
 module.exports = Mutations;
