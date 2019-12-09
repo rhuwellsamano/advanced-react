957dd83885ddfa4e05673c4185166d3d9d343358
rhuwellsamano <rhuwell.samano@gmail.com>
2019-12-09 00:31:42 -0500

    resetting cors stuff

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -9,7 +9,7 @@ function createClient({ headers }) {
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          mode: 'no-cors',
+          credentials: 'include',
         },
         headers,
       });
