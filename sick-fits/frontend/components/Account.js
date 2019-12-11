7d16745988c25bc986da355d4ad9ce10d1b686f7
rhuwellsamano <rhuwell.samano@gmail.com>
2019-12-10 19:33:42 -0500

    started me page and Account component

---
+++ sick-fits/frontend/components/Account.js
@@ -0,0 +1,8 @@
+
+const Account = props => (
+  <div>
+    <p>My Account Info</p>
+  </div>
+);
+
+export default Account;
---
+++ sick-fits/frontend/pages/me.js
@@ -0,0 +1,10 @@
+import Account from '../components/Account';
+
+
+const Me = props => (
+  <div>
+    <Account/>
+  </div>
+);
+
+export default Me;
